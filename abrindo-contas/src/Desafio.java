e5193b8a297505d432249b9f5874464800e749ac
LucasBoelter <boelter.luc4s@gmail.com>
2023-10-19 22:58:17 -0200

    Desenvolvendo projeto

---
+++ abrindo-contas/src/Desafio.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Desafio {
+
+  public static void main(String[] args) {
+    // Lendo os dados de Entrada:
+    Scanner scanner = new Scanner(System.in);
+    int numeroConta = scanner.nextInt();
+    scanner.nextLine(); // Consome a quebra de linha após a entrada do número da conta
+    String nomeTitular = scanner.nextLine();
+    double saldo = scanner.nextDouble();
+
+    //TODO: Criar uma instância de "ContaBancaria" com os valores de Entrada.
+    ContaBancaria novaConta = new ContaBancaria(numeroConta, nomeTitular, saldo);
+
+    System.out.println("Informacoes:");
+    //TODO: Imprimir as informações da conta usando o objeto criado no TODO acima.
+    System.out.println("Conta: "+novaConta.getNumero());
+    System.out.println("Titular: "+novaConta.getTitular());
+    System.out.println("Saldo: R$ "+novaConta.getSaldo());
+
+    scanner.close();
+  }
+}
+
+class ContaBancaria {
+  private int numero;
+  private String titular;
+  private double saldo;
+
+  public ContaBancaria(int numero, String titular, double saldo) {
+    this.numero = numero;
+    this.titular = titular;
+    this.saldo = saldo;
+  }
+
+  public int getNumero() {
+      return numero;
+  }
+  public String getTitular() {
+      return titular;
+  }
+  public double getSaldo() {
+      return saldo;
+  }
+}
